057ceeccf4fa61c07248a498fea2526cc3442baf
denismel2024S <165604462+denismel2024S@users.noreply.github.com>
2024-05-22 18:20:00 -0400

    Update Driver.java

--- Driver.java
+++ Driver.java
@@ -1,3 +1,30 @@
 public class Driver extends Account{
-    
+        // ~ Fields ................................................................
+    PQueue<Account> Queue;
+
+    // ~ Constructors ..........................................................
+    Driver(String Name, boolean Type) {
+        super(Name, Type);
+        Queue = new PQueue<Account>();
+    }
+
+
+    // ~Public Methods ........................................................
+    // returns the current queue of an active drive
+    public int getQueue() {
+        return Queue.size();
+    }
+
+
+    // adds a userAccount to the queue
+    public void addToQueue(UserAccount user) {
+        Queue.add(user);
+    }
+
+
+    // removes a userAccount from queue
+    public void removeFromQueue() {
+        Queue.remove();
+    }
+
 }
