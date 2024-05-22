3399166e05a69052ec43f60f0c0237f2d3b9f10f
denismel2024S <165604462+denismel2024S@users.noreply.github.com>
2024-05-22 18:22:07 -0400

    Update Account.java

--- Account.java
+++ Account.java
@@ -2,46 +2,58 @@ import java.util.PriorityQueue;
 
 public class Account {
     private String name;
-    private boolean accountType; // true driver false passenger
-    private PQueue<Account> queue;
+    private String accountType; // true driver false passenger
+    // private PQueue<Account> queue;
     private int queueSize;
-    public Account(){
-        name = "";
-        accountType = false;
-        queue = new PQueue<Account>();
-        queueSize = queue.size();
-    }
-    public Account(String name, boolean accountType){
+
+// public Account() {
+// name = "";
+// accountType = false;
+// // queue = new PQueue<Account>();
+// // queueSize = queue.size();
+// }
+
+// public Account(String name, boolean accountType){
+// this.name = name;
+// this.accountType = accountType;
+// queue = new PQueue<Account>();
+// queueSize = queue.size();
+// }
+    public Account(String name, boolean type) {
         this.name = name;
-        this.accountType = accountType;
-        queue = new PQueue<Account>();
-        queueSize = queue.size();
-    }
-    public String getName(){
-        return name;
-    }
-    public boolean getAccountType(){
-        return accountType;
-    }
-    public PQueue<Account> getQueue(){
-        return queue;
+        if (type != true) {
+            this.accountType = "Driver";
+        }
+        else {
+            this.accountType = "user";
+        }
     }
 
-    public void setName(String newName){
+
+    public String getName() {
+        return name;
+    }
+
+
+    public String getAccountType() {
+        return accountType;
+    }
+// public PQueue<Account> getQueue(){
+// // return queue;
+// }
+
+
+    public void setName(String newName) {
         name = newName;
     }
 
-    public String toString(){
+
+    public String toString() {
         StringBuilder sb = new StringBuilder();
         sb.append("Name: ");
         sb.append(name);
-        if(accountType){
-            sb.append(" Account Type: Driver");
-            sb.append(" Queue: ");
-            sb.append(queue.size());
-        }else{
-            sb.append(" Account Type: Passenger");
-        }
+        sb.append(" Account Type: " + accountType);
         return sb.toString();
     }
+
 }
